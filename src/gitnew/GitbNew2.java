7a0f4b84ed3831507b665d67c24c0268f00e885b
ahmadshahsha <110962479+ahmadshahsha@users.noreply.github.com>
2023-01-27 23:15:21 -0500

    re check done again

--- src/gitnew/GitbNew2.java
+++ src/gitnew/GitbNew2.java
@@ -1,5 +1,17 @@
 package gitnew;
 
 public class GitbNew2 {
+	
+		public static void main(String[] args) {
+			
+			int b = 4;
+			for (int a = 0; a <=3; a++) {
+				if (b == 4 )
+				System.out.println(b);
+				
+			}
+			
+		}
+		}
 
-}
+
