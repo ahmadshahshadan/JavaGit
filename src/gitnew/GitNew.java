74f2db076a80f582d20dac6442bc3edc420edc96
ahmadshahshadan <shadan300@gmail.com>
2023-01-27 20:26:57 -0600

    My fourth project

---
+++ src/gitnew/GitNew.java
@@ -0,0 +1,18 @@
+package gitnew;
+
+
+
+public class GitNew {
+	public static void main(String[] args) {
+		
+		
+		for (int a = 0; a <3; a++) {
+			for (int b = 0; b<= 3; b++) {
+			System.out.print(" *");
+		
+			}
+		System.out.println(""); 
+		}
+	}
+	}
+
